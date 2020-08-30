d5cb0e4837addb5ffe2d339bad623bc724e66e5e
Kevin <Kenvi.009@gmail.com>
2020-08-29 21:20:19 -0500

    Programa Base

---
+++ AnalizadorLexico/AnalizadorLexico/IngresarTexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AnalizadorLexico
+{
+    static class IngresarTexto
+    { 
+        public static Queue<Queue<char>> LeerTexto(String direccion) 
+        {
+            Queue<Queue<char>> lista= new Queue<Queue<char>>();
+            TextReader leer = new StreamReader(direccion);
+            string linea;
+            while ((linea = leer.ReadLine()) != null) 
+            {
+                lista.Enqueue(new Queue<char>(linea.ToList()));
+            }
+            leer.Close();
+            return lista;
+        }
+    }
+}
---
+++ AnalizadorLexico/AnalizadorLexico/TablaSimbolos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AnalizadorLexico
+{
+    static class TablaSimbolos
+    {
+        public static List<string> simbolo;
+        public static List<string> tipo;
+
+        public static void AgregarSimbolo(string simboloName, string tipoName)
+        {
+            if (!simbolo.Contains(simboloName)) 
+            {
+                simbolo.Add(simboloName);
+                tipo.Add(tipoName);
+            }
+        }
+    }
+}
